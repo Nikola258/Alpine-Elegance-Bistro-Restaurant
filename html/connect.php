cc825cda2585b4638708921b458182c685096f25
Nikola258 <nikolic07nikola@gmail.com>
2024-04-15 15:11:42 +0200

    php connection file made

--- html/connect.php
+++ html/connect.php
@@ -1,11 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
-$connection = new PDO(dsn: "mysql:dbname=Restaurant_Menu;host=mysql_db", username: "root", password: "rootpassword");
+    $connection = new PDO(dsn: "mysql:dbname=Restaurant_Menu;host=mysql_db", username: "root", password: "rootpassword");
 
-$sql = "SELECT * FROM Restaurant_Menu";
+    $sql = "SELECT * FROM Restaurant_Menu";
 
-$stmt = $connection->query($sql);
+    $stmt = $connection->query($sql);
 
-while($Restaurant_Menu = $stmt->fetch()){
-    echo "<div>" . $Restaurant_Menu["name"] . "</div>";
-}
+    while($Restaurant_Menu = $stmt->fetch()){
+        echo "<div class= 'name'>" . $Restaurant_Menu["name"] . "</div>";
+    }
 ?>
+</body>
+</html>
