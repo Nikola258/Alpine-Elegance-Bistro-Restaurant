fc374e932998b9283c3019662ab6b80a0cb02053
Nikola258 <nikolic07nikola@gmail.com>
2024-04-16 10:55:46 +0200

    nesting

--- html/connect.php
+++ html/connect.php
@@ -6,16 +6,16 @@
     <title>Document</title>
 </head>
 <body>
-<?php
-    $connection = new PDO(dsn: "mysql:dbname=Restaurant_Menu;host=mysql_db", username: "root", password: "rootpassword");
+    <?php
+        $connection = new PDO(dsn: "mysql:dbname=Restaurant_Menu;host=mysql_db", username: "root", password: "rootpassword");
 
-    $sql = "SELECT * FROM Restaurant_Menu";
+        $sql = "SELECT * FROM Restaurant_Menu";
 
-    $stmt = $connection->query($sql);
+        $stmt = $connection->query($sql);
 
-    while($Restaurant_Menu = $stmt->fetch()){
-        echo "<div class= 'name'>" . $Restaurant_Menu["name"] . "</div>";
-    }
-?>
+        while($Restaurant_Menu = $stmt->fetch()){
+            echo "<div class= 'name'>" . $Restaurant_Menu["name"] . "</div>";
+        }
+    ?>
 </body>
 </html>
