a1ac430d869e60544d7da055a83ca3ae2f30283d
Nikola258 <nikolic07nikola@gmail.com>
2024-04-17 11:24:03 +0200

    trying to link the login page with admin page

--- html/connect.php
+++ html/connect.php
@@ -16,7 +16,7 @@
 
         }
         else{
-            
+            header(header: *Location: connect.php*);
         }
 
         $connection = new PDO(dsn: "mysql:dbname=menu;host=mysql_db", username: "root", password: "rootpassword");
