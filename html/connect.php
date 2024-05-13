f75e6f66b716496645c4c2966d57b0ac8de3c3b4
Nikola258 <nikolic07nikola@gmail.com>
2024-05-13 11:57:11 +0200

    nesting

--- html/connect.php
+++ html/connect.php
@@ -11,26 +11,26 @@ $username = 'root';
 $password = 'rootpassword';
 
 
-try {
-    $pdo = new PDO("mysqli:host=$host;dbname=$dbname", $username, $password);
+    try {
+        $pdo = new PDO("mysqli:host=$host;dbname=$dbname", $username, $password);
     
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
 
     
-    $stmt = $pdo->prepare("SELECT * FROM Menu");
-    $stmt->execute();
+        $stmt = $pdo->prepare("SELECT * FROM Menu");
+        $stmt->execute();
 
     
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     
-    foreach ($results as $row) {
-        echo "<div class='name'>" . $row['name'] . "</div>";
+        foreach ($results as $row) {
+            echo "<div class='name'>" . $row['name'] . "</div>";
+        }
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
     }
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
 
 ?>
 
