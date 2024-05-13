1c47c0ca79fdbd235666018393f4c9f9425d6067
Nikola258 <nikolic07nikola@gmail.com>
2024-05-13 11:56:26 +0200

    deleting some text

--- html/Foods.php
+++ html/Foods.php
@@ -2,16 +2,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <!-- Important to make website responsive -->
+    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restaurant Website</title>
 
-    <!-- Link our CSS file -->
+    
     <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    <!-- Navbar Section Starts Here -->
+    
     <section class="navbar">
         <div class="container">
             <div class="logo">
@@ -37,9 +37,9 @@
             <div class="clearfix"></div>
         </div>
     </section>
-    <!-- Navbar Section Ends Here -->
+    
 
-    <!-- fOOD sEARCH Section Starts Here -->
+    
     <section class="food-search text-center">
         <div class="container">
             
@@ -50,9 +50,8 @@
 
         </div>
     </section>
-    <!-- fOOD sEARCH Section Ends Here -->
+    
 
-    <!-- fOOD MEnu Section Starts Here -->
     <section class="food-menu">
         <div class="container">
             <h2 class="text-center">Food Menu</h2>
@@ -170,9 +169,9 @@
             <a href="#">See All Foods</a>
         </p>
     </section>
-    <!-- fOOD Menu Section Ends Here -->
+    
 
-    <!-- social Section Starts Here -->
+
     <section class="social">
         <div class="container text-center">
             <ul>
@@ -188,15 +187,15 @@
             </ul>
         </div>
     </section>
-    <!-- social Section Ends Here -->
+    
 
-    <!-- footer Section Starts Here -->
+
     <section class="footer">
         <div class="container text-center">
             <p>All rights reserved. Designed By <a href="#">Nikola</a></p>
         </div>
     </section>
-    <!-- footer Section Ends Here -->
+    
 
 </body>
 </html>
--- html/connect.php
+++ html/connect.php
@@ -1,31 +1,68 @@
 <?php
 session_start();
 ?>
+
+<?php
+
+
+$host = 'localhost';
+$dbname = 'Menu';
+$username = 'root';
+$password = 'rootpassword';
+
+
+try {
+    $pdo = new PDO("mysqli:host=$host;dbname=$dbname", $username, $password);
+    
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+
+    
+    $stmt = $pdo->prepare("SELECT * FROM Menu");
+    $stmt->execute();
+
+    
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    
+    foreach ($results as $row) {
+        echo "<div class='name'>" . $row['name'] . "</div>";
+    }
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- Link our CSS file -->
+    
     <link rel="stylesheet" href="css/style.css">
 
     <title>Document</title>
 </head>
 <body>
+
 <?php
 $servername = "localhost";
 $username = "root";
 $password = "rootpassword";
 $dbname = "Menu";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+try {
+    $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
-    
+
     <ul>
         <li><a href="#">Verwijder gerecht</a></li>
         <li><a href="Add.php">Voeg nieuwe gerecht toe</a></li>
