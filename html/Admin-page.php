8d6663c7d197d72536672118e4a4430c6eae50f5
Nikola258 <nikolic07nikola@gmail.com>
2024-05-13 16:10:43 +0200

    changed some stuff

--- html/Admin-page.php
+++ html/Admin-page.php
@@ -17,19 +17,27 @@
 <body>
 
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "rootpassword";
-$dbname = "Menu";
+$host = 'mysql_db';
+$dbname = 'Menu';
+$username = 'root';
+$password = 'rootpassword';
 
 try {
-    $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
+
+    $stmt = $pdo->prepare("SELECT * FROM Menu1");
+    $stmt->execute();
+
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($results as $row) {
+        echo "<div class='name'>" . htmlspecialchars($row['name']) . "</div>";
+    }
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
-
 ?>
 
     <ul>
